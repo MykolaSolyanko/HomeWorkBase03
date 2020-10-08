4d3f1e2be74602a590a257a0305a10d3442d31c1
gorik333 <50589173+gorik333@users.noreply.github.com>
2020-10-08 22:41:24 +0300

    Update George_Kutsurais_1_homework.cpp

--- George_Kutsurais_1_homework.cpp
+++ George_Kutsurais_1_homework.cpp
@@ -4,30 +4,32 @@
 int main() {
 
   std::cout << "\t\t\tCalculating\n";
-  std::cout << "Please enter number in range [2..255] --> ";
+  std::cout << "a = 4, c = 2" << std::endl;
+  std::cout << "Please enter b in range [2..255] --> ";
   int b{};
   std::cin >> b;
-  double a = {4}, c = {2};
+  int a = {4}, c = {2};
   bool run = false;
-  int d = b * b - 4 * a * c;
-  if (d > 0) {
-    while (!run) {
-      if (b < 256 && b > 1 && pow(b, 2) > 4 * a * c) {
-        double x1 = ((-b) + sqrt(d)) / (2 * a);
-        double x2 = ((-b) - sqrt(d)) / (2 * a);
 
-        std::cout << "x1 = " << x1 << std::endl;
-        std::cout << "x2 = " << x2 << std::endl;
-        break;
-      } else if (pow(b, 2) < 4 * a * c) {
-        std::cout << "Cannot calculate if sqrt b^2 < 4 * a * c\n";
-        std::cout << "Please enter another number.\n";
-        std::cin >> b;
-      } else {
-        std::cout << "You entered an incorrect number, try entering the number "
-                     "again. \n";
-        std::cin >> b;
-      }
+  while (!run) {
+    int d = pow(b, 2) - 4 * a * c;
+    if (b < 2 || b > 255) {
+      std::cout << "You entered an incorrect number, try entering the number "
+                   "again. \n";
+      std::cin >> b;
+    }
+
+    else if (d < 0) {
+      std::cout << "Cannot calculate if d < 0\n";
+      std::cout << "Please enter another number.\n";
+      std::cin >> b;
+    } else if (d > 0) {
+      double x1 = ((-b) + sqrt(d)) / (2 * a);
+      double x2 = ((-b) - sqrt(d)) / (2 * a);
+
+      std::cout << "x1 = " << x1 << std::endl;
+      std::cout << "x2 = " << x2 << std::endl;
+      break;
     }
   }
 }
