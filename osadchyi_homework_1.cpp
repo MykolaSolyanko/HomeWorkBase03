7cde23d069df37b4a091c0dff6d42c79f9585f52
NikOsadchyi <kol.osadchiy@gmail.com>
2020-10-08 19:21:30 +0300

    Updated while scope

--- osadchyi_homework_1.cpp
+++ osadchyi_homework_1.cpp
@@ -12,14 +12,14 @@ int main() {
                    "leave 0."
             << std::endl;
 
-  short a{};
-  short b{};
-  short c{};
-  bool a_range{false};
-  bool b_range{false};
-  bool c_range{false};
-
   while (atmpts) {
+    short a{};
+    short b{};
+    short c{};
+    bool a_range{false};
+    bool b_range{false};
+    bool c_range{false};
+
     std::cin >> a >> b >> c;
     a_range = a >= lwr_bndr && a <= uppr_bndr;
     b_range = b >= lwr_bndr && b <= uppr_bndr;
@@ -30,35 +30,36 @@ int main() {
         atmpts != 0) // check for attempts is needed so that not to print this
                      // message at the end
       std::cout
-          << "One of your values is out of range. Please, please try again"
+          << "One of your values is out of range. Please, try again."
           << std::endl;
-    else
+    else if (a_range && b_range && c_range) {
+      double x_1{};
+      double x_2{};
+      double discr = b * b - 4 * a * c;
+      double mul_2_a = 2 * a;
+
+      if (discr > 0) {
+        double sqrt_discr = std::sqrt(discr);
+
+        x_1 = (-b + sqrt_discr) / mul_2_a;
+        x_2 = (-b - sqrt_discr) / mul_2_a;
+
+        std::cout << "x1 = " << x_1 << std::endl;
+        std::cout << "x2 = " << x_2 << std::endl;
+      } else if (discr == 0) {
+        x_1 = -(b / mul_2_a);
+
+        std::cout << "x1 = x2 = " << x_1 << std::endl;
+      } else {
+        std::cout << "Discriminant < 0. The equation does not have solutions."
+                  << std::endl;
+      }
       break;
+    } else {
+      std::cout << "Sorry, no more attempts." << std::endl;
+      break;
+    }
   }
 
-  if (a_range && b_range && c_range) {
-    double x_1{};
-    double x_2{};
-    double discr = b * b - 4 * a * c;
-    double mul_2_a = 2 * a;
-
-    if (discr > 0) {
-      double sqrt_discr = std::sqrt(discr);
-
-      x_1 = (-b + sqrt_discr) / mul_2_a;
-      x_2 = (-b - sqrt_discr) / mul_2_a;
-
-      std::cout << "x1 = " << x_1 << std::endl;
-      std::cout << "x2 = " << x_2 << std::endl;
-    } else if (discr == 0) {
-      x_1 = -(b / mul_2_a);
-
-      std::cout << "x1 = x2 = " << x_1 << std::endl;
-    } else {
-      std::cout << "Discriminant < 0. The equation does not have solutions."
-                << std::endl;
-    }
-  } else
-    std::cout << "Sorry, no more attempts." << std::endl;
   return 0;
 }
