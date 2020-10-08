dc16096016135212a914cf7ff446c3eaaaaadfe2
Мороз Дмитрий Викторович <moroz@radmirtech.com.ua>
2020-10-08 09:26:53 +0300

    Fixed pull request issues

--- Moroz_Homework_1.cpp
+++ Moroz_Homework_1.cpp
@@ -2,27 +2,28 @@
 #include <iostream>
 #include <limits>
 
-constexpr uint16_t dividendMultiply(4);
-constexpr uint16_t dividerMultiply(2);
+constexpr uint16_t dividendMultiply{4};
+constexpr uint16_t dividerMultiply{2};
 
-constexpr uint16_t numberOfAttempts(3);
+constexpr uint16_t numberOfAttempts{3};
+constexpr uint16_t minValue{2};
 
 int main() {
-  int16_t a(0);
-  int16_t b(0);
-  int16_t c(0);
+  int16_t a{};
+  int16_t b{};
+  int16_t c{};
 
-  uint16_t attemptsCounter = 0;
+  uint16_t attemptsCounter{};
 
   while (attemptsCounter < numberOfAttempts) {
-    std::cout << "Please, enter 3 variables (a, b, c) separated by space. "
+    std::cout << "Please, enter " << numberOfAttempts << " variables (a, b, c) separated by space. "
                  "Variables must be in range 2.."
               << std::numeric_limits<uint16_t>::max() << std::endl;
     std::cin >> a >> b >> c;
 
-    if ((a >= 2) && (a < std::numeric_limits<uint16_t>::max())) {
-      if ((b >= 2) && (a < std::numeric_limits<uint16_t>::max())) {
-        if ((c >= 2) && (a < std::numeric_limits<uint16_t>::max())) {
+    if ((a >= minValue) && (a < std::numeric_limits<uint16_t>::max())) {
+      if ((b >= minValue) && (a < std::numeric_limits<uint16_t>::max())) {
+        if ((c >= minValue) && (a < std::numeric_limits<uint16_t>::max())) {
           std::cout << "All variables are correct." << std::endl;
           break;
         } else {
@@ -40,10 +41,11 @@ int main() {
   }
 
   if (attemptsCounter < numberOfAttempts) {
-    auto discriminant = b * b - dividendMultiply * a * c;
+    const auto divider = dividerMultiply * a;
+    const auto discriminant = (b * b) - (divider * c);
     if (discriminant > 0) {
-      auto x1 = (-b + sqrt(discriminant)) / (dividerMultiply * a);
-      auto x2 = (-b - sqrt(discriminant)) / (dividerMultiply * a);
+      auto x1 = (-b + sqrt(discriminant)) / divider;
+      auto x2 = (-b - sqrt(discriminant)) / divider;
       std::cout << "x1 = " << x1 << ", x2 = " << x2 << std::endl;
     } else if (discriminant == 0) {
       auto x1 = -b / (dividerMultiply * a);
