ad167673fc5b62b03c64fb6f9726bd8abf57ea28
Dmytro Filippov <70589756+dim-Fi@users.noreply.github.com>
2020-10-10 18:29:31 +0300

    Update Filippov_homework1.cpp

--- Filippov_homework1.cpp
+++ Filippov_homework1.cpp
@@ -4,50 +4,62 @@
 constexpr int val1 = 4;
 constexpr int val2 = 2;
 
- //функция для вычиления корней квадратного уравнения
-void quadr_equation( double a, double b, double c) { 
+//функция для вычиления корней квадратного уравнения
+void quadr_equation(double a, double b, double c) {
+
   if (a == 0) {
-    std::cout << "деление на ноль\n";
+    std::cout << "\nделение на ноль\n";
     return;
   }
-  double discrim = b * b - 4 * a * c; //расчет дискриминанта
+  const double val3 = val2 * a;
+  const double discrim = b * b - 4 * a * c; //расчет дискриминанта
 
   if (discrim < 0) {
-    std::cout << "нет вещественных корней\n";
+    std::cout << "\nнет вещественных корней\n";
     return;
   }
-  auto x1 = (-b + sqrt(b * b - val1 * a * c)) / (val2 * a);
-  auto x2 = (-b - sqrt(b * b - val1 * a * c)) / (val2 * a);
+  auto x1 = (-b + sqrt(discrim)) / val3;
+  auto x2 = (-b - sqrt(discrim)) / val3;
 
-  std::cout << "Для квадратного уравнения " << a << "x^2+" << b << "x+" << c ="0\n"
+  std::cout << "\nДля квадратного уравнения " << a << "x^2+" << b << "x+" << c
+            << "=0\n"
             << "x1=" << x1 << "   x2=" << x2 << '\n';
 }
 
 int main() {
-  double a{};
-  double b{};
-  double c{};
 
   char ans{};
 
   do {
+    constexpr int max_val = 1000;
+    constexpr int min_val = -1000;
+
+    double a{};
+    double b{};
+    double c{};
+
     std::cout
         << "Для квадратного уравнения вида ax^2+bx+c=0\n"
         << "введите a, b, c - вещественные числа в диапазоне [-1000..1000]\n";
 
     std::cin >> a >> b >> c;
-
-    if (a > 1000 || a < -1000 || b > 1000 || b < -1000 || c > 1000 ||
-        c < -1000) {
+    if (std::cin.fail()) {
+      std::cout << "некорректный ввод\n";
+      break;
+    }
+    if ((a > max_val) || (b > max_val) || (c > max_val) || (a < min_val) ||
+        (b < min_val) || (c < min_val)) {
       std::cout << "некорректное значение\n";
+      break;
     }
     quadr_equation(a, b, c);
 
-    std::cout << "Если хотите повторить вычисление  введите 'y'или 'Y'\n"
-              << "если нет - любой другой симол\n";
+    std::cout << "\nЕсли хотите повторить вычисление  введите 'y'или 'Y'\n"
+              << "если нет - любой другой символ\n\n";
     std::cin >> ans;
 
   } while (ans == 'y' || ans == 'Y');
 
   return 0;
 }
+
