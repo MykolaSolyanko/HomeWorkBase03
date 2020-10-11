eb1b116bd247e29c3cc7c905ddd21f8ce557c9f6
GKuzzinshtern <72338984+GKuzzinshtern@users.noreply.github.com>
2020-10-11 22:08:59 +0300

    Yuriy_Kuznetsov_HomeWork2_ReverseNumbers

---
+++ Yuriy_Kuznetsov_HomeWork2_ReverseNumbers.cpp
@@ -0,0 +1,24 @@
+// Обратные числа
+#include <iostream>
+
+int main(int argc, char *argv[]) {
+  constexpr std::int32_t ci_KOEF{10};
+  std::int32_t iInNumber{};
+
+  std::cout << "Enter integer number: ";
+  if (!(std::cin >> iInNumber)) {
+    std::cout << "Uncorrect number!";
+  }
+
+  // Алгоритм честно подсмотрел в интернете :)
+  // Изначально хотел все запихать в вектор или строку и прочитать реверсным
+  // итератором
+  std::int32_t iRevNumber{};
+  while (iInNumber) {
+    iRevNumber = iRevNumber * ci_KOEF + iInNumber % ci_KOEF;
+    iInNumber /= ci_KOEF;
+  }
+
+  std::cout << iRevNumber << '\n';
+  return 0;
+}
