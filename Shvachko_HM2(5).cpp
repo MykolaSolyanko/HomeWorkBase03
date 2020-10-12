5f54b5c581e1321b2bb310cb0d7fadf3fb6a74cb
Nazar <70706894+z0r0vich@users.noreply.github.com>
2020-10-12 18:58:24 +0300

    Create Shvachko_HM2(5).cpp

---
+++ Shvachko_HM2(5).cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+int main() {
+  constexpr int16_t Max_Size{50};
+  constexpr int16_t MinRange{-60};
+  constexpr int16_t MaxRange{90};
+
+  std::cout << "Enter list integers from " << MinRange << " to " << MaxRange
+            << "\n"
+            << "(no more 50 numbers). At the end of input put 0 \n";
+
+  int Sum{};
+
+  for (int i{1}; i < Max_Size; ++i) {
+    int32_t Numb{};
+    std::cin >> Numb;
+    if (!std::cin || Numb == 0) {
+      break;
+    }
+    if (Numb < MinRange || Numb > MaxRange) {
+      std::cout << "Incorrect number\n";
+      break;
+    }
+    if (i % 2 != 0) {
+      Sum += Numb;
+    }
+  }
+  std::cout << "sum of odd numbers =  " << Sum << '\n';
+
+  return 0;
+}
