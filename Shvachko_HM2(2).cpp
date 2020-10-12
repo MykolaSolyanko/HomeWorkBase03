e9c17e9a2b949ffff1a79b121a7a43af99eacc31
Nazar <70706894+z0r0vich@users.noreply.github.com>
2020-10-12 15:36:30 +0300

    Create Shvachko_HM2(2).cpp

---
+++ Shvachko_HM2(2).cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+unsigned getSum(unsigned value) {
+  unsigned result = value % 10;
+  while (value /= 10)
+    result += value % 10;
+  return result;
+}
+
+int main() {
+  std::cout << "Enter your ticket number --->";
+  std::int32_t Numb{};
+  std::cin >> Numb;
+  std::int16_t Res1 = Numb / 1000;
+  std::int16_t Res2 = Numb % 1000;
+  if (getSum(Res1) != getSum(Res2)) {
+    std::cout << ":(";
+    return 1;
+  }
+  std::cout << "You win!";
+  return 0;
+}
