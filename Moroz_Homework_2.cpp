863393b55d4865be168fbbad7e11dc57fb1329c5
kramp18 <moroz.d.v.94@gmail.com>
2020-10-12 22:01:53 +0300

    Add some functionality

--- Moroz_Homework_2.cpp
+++ Moroz_Homework_2.cpp
@@ -3,7 +3,7 @@
 void getSumAndAverage() {
   std::cout << R"(Please, enter some integer value to calculate
           the sum of digits and it's average: )";
-  long inputValue{};
+  long long inputValue{};
   std::cin >> inputValue;
 
   long long sumOfDigits{};
@@ -32,8 +32,8 @@ void luckyTicket() {
   }
 
   int temp{luckyTicketValue};
-  int counter{};
-  while ((temp /= 10) != 0) {
+  int counter{1};
+  while ((temp /= 10) > 0) {
     counter++;
   }
 
@@ -41,10 +41,64 @@ void luckyTicket() {
     std::cerr << "The number of digits is different from 6.";
     return;
   }
+
+  uint16_t firstPart{};
+  uint32_t divider{100000};
+  for (size_t i = 0; i < 3; i++) {
+    firstPart += (luckyTicketValue / divider);
+    luckyTicketValue %= divider;
+    divider /= 10;
+  }
+
+  uint16_t secondPart{};
+  for (size_t i = 0; i < 3; i++) {
+    secondPart += (luckyTicketValue / divider);
+    luckyTicketValue %= divider;
+    divider /= 10;
+  }
+
+  if (firstPart != secondPart) {
+    std::cout << "This number is not lucky:(" << std::endl;
+    return;
+  }
+
+  std::cout << "Congratulations! Your number is lucky!" << std::endl;
 }
 
 int main() {
-  getSumAndAverage();
-  luckyTicket();
-  return 0;
+  //  getSumAndAverage();
+  //  luckyTicket();
+
+  while (true) {
+    std::cout << "Choose your function. Enter one of the numbers below. To "
+                 "exit enter -1."
+              << std::endl;
+    std::cout << "1 - Sum and Average." << std::endl;
+    std::cout << "2 - Lucky ticket." << std::endl;
+    std::cout << "-----> ";
+    int16_t marker{};
+    std::cin >> marker;
+    switch (marker) {
+    case 1:
+      std::cout << std::endl;
+      getSumAndAverage();
+      std::cout << std::endl;
+      break;
+    case 2:
+      std::cout << std::endl;
+      luckyTicket();
+      std::cout << std::endl;
+      break;
+    case -1:
+      std::cout << std::endl;
+      std::cout << "Bye!" << std::endl;
+      return 0;
+      break;
+    default:
+      std::cout << "This function is not presented." << std::endl;
+      break;
+    }
+  }
+
+  return -1;
 }
