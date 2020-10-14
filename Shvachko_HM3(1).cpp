95825c605783a3fdfc9a745663bb46771c09ae9a
Nazar <70706894+z0r0vich@users.noreply.github.com>
2020-10-14 19:47:46 +0300

    Create Shvachko_HM3(1).cpp

---
+++ Shvachko_HM3(1).cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+
+int main() {
+
+  std::cout << "Enter line which want to reverse ---> ";
+  std::string s;
+  std::cin >> s;
+
+  for (int i = s.length() - 1; i >= 0; i--) {
+    std::cout << s[i];
+  }
+  return 0;
+}
