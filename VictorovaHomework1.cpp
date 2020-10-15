5e1df64264bb3fb49c885c1c218c9e9c82d3096b
yaroslava15 <gimnastka022013@gmail.com>
2020-10-15 14:51:15 +0300

    Update ViktorovaHomework1.cpp

--- VictorovaHomework1.cpp
+++ VictorovaHomework1.cpp
@@ -2,25 +2,28 @@
 #include <iostream>
 
 int main() {
-    constexpr auto coef1{ 4 };
-    constexpr auto coef2{ 2 };
-    auto a{ 0 };
-    auto b{ 0 };
-    auto c{ 0 };
-    auto D{ 0.0 };
+    constexpr int kMinRange{ -2 };
+    constexpr int kMaxRange{ 365 };
+
     auto x1{ 0.0 };
     auto x2{ 0.0 };
 
-    std::cout << "Please enter coefficient a in range [2...365] -->" << std::endl;
+    std::cout << "Please enter coefficient a (in the range from " << kMaxRange
+        << "to" << kMaxRange << ")" << std::endl;
+    auto a{ 0 };
     std::cin >> a;
     if (a >= 2 && a <= 365) {
         std::cout << "You entered a correct number" << std::endl;
     }
     else {
         std::cout << "You entered incorrect number, try to enter the number again"
+
             << std::endl;
+        return -1;
     }
-    std::cout << "Please enter coefficient b in range [2...365] -->" << std::endl;
+    std::cout << "Please enter coefficient b (in the range from" << kMinRange
+        << "to" << kMaxRange << ")" << std::endl;
+    auto b{ 0 };
     std::cin >> b;
     if (b >= 2 && b <= 365) {
         std::cout << "You entered a correct number" << std::endl;
@@ -28,8 +31,11 @@ int main() {
     else {
         std::cout << "You entered incorrect number, try to enter the number again"
             << std::endl;
+        return -1;
     }
-    std::cout << "Please enter coefficient c in range [2...365] -->" << std::endl;
+    std::cout << "Please enter coefficient c (in the range " << kMinRange << "to"
+        << kMaxRange << ")" << std::endl;
+    auto c{ 0 };
     std::cin >> c;
     if (c >= 2 && c <= 365) {
         std::cout << "You entered a correct number" << std::endl;
@@ -37,15 +43,15 @@ int main() {
     else {
         std::cout << "You entered incorrect number, try to enter the number again"
             << std::endl;
+        return -1;
     }
+    const long double kD = (b * b) - 4 * a * c;
+    std::cout << "D=" << kD << std::endl;
 
-    D = pow(b, 2) - coef1 * a * c;
-    std::cout << "D=" << D << std::endl;
-
-    if (D >= 0) {
-        x1 = (-b + sqrt(pow(b, 2) - coef1 * a * c)) / (coef2 * a);
+    if (kD >= 0) {
+        x1 = (-b + sqrt(kD)) / (2 * a);
         std::cout << "x1=" << x1 << std::endl;
-        x2 = (-b - sqrt(pow(b, 2) - coef1 * a * c)) / (coef2 * a);
+        x2 = (-b - sqrt(kD)) / (2 * a);
         std::cout << "x2=" << x2 << std::endl;
     }
     else {
