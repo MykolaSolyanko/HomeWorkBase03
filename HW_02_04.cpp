db63f83b2ad566639c5cf91efcee9ee6255da23f
padawanYoung <veyRon.sportcar@gmail.com>
2020-10-17 20:28:47 +0300

    HW_02_04 was started

---
+++ HW_02_04.cpp
@@ -0,0 +1,17 @@
+#include <cmath>
+#include <cstdbool>
+#include <cstdint>
+#include <cstdlib>
+#include <iostream>
+
+int summ_odd();
+
+int main() {
+  /**
+   * @brief
+   *
+   */
+  return EXIT_SUCCESS;
+}
+
+int summ_odd() {}
