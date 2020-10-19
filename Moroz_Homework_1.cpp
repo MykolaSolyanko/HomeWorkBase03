ba86aa97f5f859fae4002049aa29f1744eb3fa6b
Мороз Дмитрий Викторович <moroz@radmirtech.com.ua>
2020-10-19 15:49:06 +0300

    Fixed pull request issues №2

--- Moroz_Homework_1.cpp
+++ Moroz_Homework_1.cpp
@@ -18,37 +18,35 @@ int main() {
   while (attemptsCounter < numberOfAttempts) {
     std::cout << "Please, enter " << numberOfAttempts << " variables (a, b, c) separated by space. "
                  "Variables must be in range 2.."
-              << std::numeric_limits<uint16_t>::max() << std::endl;
+              << std::numeric_limits<int16_t>::max() << std::endl;
     std::cin >> a >> b >> c;
 
-    if ((a >= minValue) && (a < std::numeric_limits<uint16_t>::max())) {
-      if ((b >= minValue) && (a < std::numeric_limits<uint16_t>::max())) {
-        if ((c >= minValue) && (a < std::numeric_limits<uint16_t>::max())) {
+    if ((a >= minValue) && (a < std::numeric_limits<int16_t>::max())) {
+      if ((b >= minValue) && (a < std::numeric_limits<int16_t>::max())) {
+        if ((c >= minValue) && (a < std::numeric_limits<int16_t>::max())) {
           std::cout << "All variables are correct." << std::endl;
           break;
         } else {
           std::cout << "c is out of range." << std::endl;
-          attemptsCounter++;
         }
       } else {
         std::cout << "b is out of range." << std::endl;
-        attemptsCounter++;
       }
     } else {
       std::cout << "a is out of range." << std::endl;
-      attemptsCounter++;
     }
+    attemptsCounter++;
   }
 
   if (attemptsCounter < numberOfAttempts) {
     const auto divider = dividerMultiply * a;
     const auto discriminant = (b * b) - (divider * c);
     if (discriminant > 0) {
-      auto x1 = (-b + sqrt(discriminant)) / divider;
-      auto x2 = (-b - sqrt(discriminant)) / divider;
+      const auto x1 = (-b + sqrt(discriminant)) / divider;
+      const auto x2 = (-b - sqrt(discriminant)) / divider;
       std::cout << "x1 = " << x1 << ", x2 = " << x2 << std::endl;
     } else if (discriminant == 0) {
-      auto x1 = -b / (dividerMultiply * a);
+      const auto x1 = -b / (dividerMultiply * a);
       std::cout << "x1 = x2 = " << x1 << std::endl;
     } else
       std::cout << "Discriminant is less than 0." << std::endl;
