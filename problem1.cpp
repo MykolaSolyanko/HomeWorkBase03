4f21b92605fba291b3d014bc1538965f1a4bf94e
Dmytro Filippov <70589756+dim-Fi@users.noreply.github.com>
2020-10-21 18:39:27 +0300

    Add two constexpr and check input, changed name const variable Digit_in_Number

--- problem1.cpp
+++ problem1.cpp
@@ -2,22 +2,29 @@
 #include <iostream>
 
 int main() {
-
-  std::cout << "Enter the integer in scope from 1 to 1000000\n";
-
+ 
+  constexpr int Lower_Limit {1};
+  constexpr int Upper_Limit {1000000};
+  
+  std::cout << "Enter the integer in scope from "<< Lower_Limit <<" to "<< Upper_Limit <<'\n';
+  
   int number{};
   std::cin >> number;
+  if(number < Lower_Limit || number > Upper_Limit){
+    std::cout<<"incorrect input\n";
+    return 1;
+  }
 
-  const int n = std::log10(number) + 1;
+  const int Digit_in_Number = std::log10(number) + 1;
   int sum{};
 
-  for (int i{}; i < n; ++i) {
+  for (int i=0; i < Digit_in_Number; ++i) {
     auto val = number % 10;
     sum += val;
     number /= 10;
   }
 
-  auto average = static_cast<double>(sum) / n;
+  auto average = static_cast<double>(sum) / Digit_in_Number;
 
   std::cout << "Sum of digits in number  equal " << sum << '\n'
             << "Average is  " << average << '\n';
