6b5c57e93473991d1b302fa8464ef809e4338237
Nazar <70706894+z0r0vich@users.noreply.github.com>
2020-10-21 14:22:51 +0300

    Update Shvachko_HM2(3).cpp

--- Shvachko_HM2(3).cpp
+++ Shvachko_HM2(3).cpp
@@ -1,24 +1,21 @@
 // inverse number
+#include <cmath>
 #include <iostream>
 
 int main(int argc, char *argv[]) {
+  std::cout << "Enter value, which want to inverse-->";
   constexpr std::int32_t k{10};
   std::int32_t Number{};
+  std::cin >> Number;
   // check for correct input(int32_t)
-  if (!(std::cin) >> Number) {
+  if (!(std::cin) >> (Number)) {
     std::cout << "Uncorrect";
     return 1;
   }
 
-  // firstly, did with string
-  //  std::string Number;
-  //	std::cin >> Number;
-  //	std::cout << std::string(Number.rbegin(), Nubmer.rend()) << '\n';
-
-  // found on the internet, but its work
   std::int32_t RevNumber{};
-  while (Number) {
-    RevNumber = RevNumber * k + Number % k;
+  while (abs(Number) > 0) {
+    RevNumber = RevNumber * k + abs(Number) % k;
     Number /= k;
   }
 
