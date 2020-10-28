e84ff0b2857c0dd525dbe8c18756f90fd6bdeb68
GKuzzinshtern <72338984+GKuzzinshtern@users.noreply.github.com>
2020-10-28 20:28:39 +0200

    Yuriy_Kuznetsov_HomeWork3_Reverse(Remake)
    Redone taking into account the comments on the second homework

--- Yuriy_Kuznetsov_HomeWork3_Reverse.cpp
+++ Yuriy_Kuznetsov_HomeWork3_Reverse.cpp
@@ -1,44 +1,33 @@
+//программа revers для строки
 #include <iostream>
-#include <string>
 
-void vReverseAsString() {
-  std::cout << "Enter the string: ";
-  std::string sInData{};
-  std::getline(std::cin, sInData);
-  std::cout << "Reversed string: ";
-  for (auto p = sInData.rbegin(); p < sInData.rend(); ++p) {
-    std::cout << *p;
-  }
-}
-
-void vReverseAsCharString() {
+int main() {
   constexpr uint32_t cuiMAXCHAR{256};
   constexpr char ccENDOFSTRING{'\0'};
   constexpr uint32_t cuiHALF{2};
-  char acInBuffer[cuiMAXCHAR];
+  char acBuffer[cuiMAXCHAR];
 
-  std::cout << "Enter the string: ";
-  std::cin >> acInBuffer; // without any checking
+  std::cout << "Enter the string you want to reverse: ";
+  std::cin >> acBuffer; // without any checking
 
-  for (uint32_t uiCharIndex = 0; uiCharIndex < cuiMAXCHAR; ++uiCharIndex) {
-    if (acInBuffer[uiCharIndex] != ccENDOFSTRING) {
+  for (size_t uiCharIndex = 0; uiCharIndex < cuiMAXCHAR; ++uiCharIndex) {
+    if (acBuffer[uiCharIndex] != ccENDOFSTRING) {
       continue;
     }
-    for (uint32_t uiHelpIndex = 0; uiHelpIndex < (uiCharIndex / cuiHALF);
+    for (size_t uiHelpIndex = 0; uiHelpIndex < (uiCharIndex / cuiHALF);
          ++uiHelpIndex) {
-      std::swap(acInBuffer[uiHelpIndex],
-                acInBuffer[uiCharIndex - 1 - uiHelpIndex]);
+      char cTempSymbol{acBuffer[uiHelpIndex]};
+      acBuffer[uiHelpIndex] = acBuffer[uiCharIndex - 1 - uiHelpIndex];
+      acBuffer[uiCharIndex - 1 - uiHelpIndex] = cTempSymbol;
+
+      /*      std::swap(acBuffer[uiHelpIndex],
+                      acBuffer[uiCharIndex - 1 - uiHelpIndex]);
+      */
     }
     break;
   }
   std::cout << "Reversed string: ";
-  std::cout << acInBuffer << '\n';
-}
-
-int main() {
-  vReverseAsString();
-
-  vReverseAsCharString();
+  std::cout << acBuffer << '\n';
 
   return 0;
 }
