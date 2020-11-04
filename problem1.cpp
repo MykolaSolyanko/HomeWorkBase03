a26b66b5615789ef62800010bd739af619056166
Dmytro Filippov <70589756+dim-Fi@users.noreply.github.com>
2020-11-04 19:35:20 +0200

    Filippov homework4

---
+++ problem1.cpp
@@ -0,0 +1,148 @@
+#include <chrono>
+#include <iostream>
+#include <random>
+
+void fill_array_random(int *begin, const int *const end);
+void fill_array_by_hand(int *begin, size_t *const end);
+void bubble_sort(int *begin, int *const end);
+void quick_sort(int *begin, int *end);
+
+int main() {
+  constexpr uint16_t Lower_Scope{1};
+  constexpr uint32_t Upper_Scope{100000};
+
+  std::cout << "To compare bubble sort and quicksort algorithm,\n"
+            << "enter size sortable array (positive integer "
+            << "in scope[" << Lower_Scope << ".." << Upper_Scope << "])\n";
+
+  size_t Size_Array{};
+  std::cin >> Size_Array;
+  if (!std::cin || Size_Array < Lower_Scope || Size_Array > Upper_Scope) {
+    std::cout << "incorrect input\n";
+    return 1;
+  }
+
+  int array[Size_Array];
+
+  std::cout << "Choose a way to fill the array:\n"
+            << "1 - fill random\n"
+            << "2 - fill by hand\n";
+
+  constexpr char Random{'1'};
+  constexpr char ByHand{'2'};
+
+  char choose{};
+  std::cin >> choose;
+  if (std::cin) {
+    switch (choose) {
+    case Random:
+      fill_array_random(array, array + Size_Array);
+      break;
+    case ByHand:
+      fill_array_by_hand(array, &Size_Array);
+      break;
+    default:
+      std::cout << "incorrect input\n";
+      return 2;
+      break;
+    }
+  }
+
+  int copy_array[Size_Array];
+
+  for (size_t i = 0; i < Size_Array; ++i) {
+    copy_array[i] = array[i];
+  }
+
+  std::cout << "Comparison execution time of sorts " << Size_Array
+            << " numbers: \n";
+
+  auto start = std::chrono::steady_clock::now();
+  quick_sort(array, array + Size_Array);
+  auto end = std::chrono::steady_clock::now();
+  std::chrono::duration<double> elapsed_seconds = end - start;
+  std::cout << "Quicksort elapsed time: " << elapsed_seconds.count() << "s\n";
+
+  auto start1 = std::chrono::steady_clock::now();
+  bubble_sort(copy_array, copy_array + Size_Array);
+  auto end1 = std::chrono::steady_clock::now();
+  std::chrono::duration<double> elapsed_seconds1 = end1 - start1;
+  std::cout << "Bubble sort elapsed time: " << elapsed_seconds1.count()
+            << "s\n";
+
+  return 0;
+}
+
+void fill_array_random(int *begin, const int *const end) {
+  if (begin == nullptr || end == nullptr) {
+    std::cout << "One of the argument is null\n";
+    return;
+  }
+  std::random_device rd;
+  while (begin != end) {
+    *begin++ = rd() % 100;
+  }
+  std::cout << '\n';
+}
+
+void fill_array_by_hand(int *begin, size_t *const end) {
+  if (begin == nullptr || end == nullptr) {
+    std::cout << "One of the argument is null\n";
+    return;
+  }
+  std::cout << "Enter " << *end << " integers in the array\n";
+
+  for (size_t i = 0; i < *end; ++i) {
+    int number{};
+    std::cin >> number;
+    if (std::cin) {
+      *begin++ = number;
+    }
+  }
+  std::cout << '\n';
+}
+
+void bubble_sort(int *begin, int *const end) {
+  if (begin == nullptr || end == nullptr) {
+    std::cout << "One of the argument is null\n";
+    return;
+  }
+  bool swap = true;
+  for (int *i = end; i > begin && swap; --i) {
+    swap = false;
+    for (int *n = begin; n < i - 1; ++n) {
+      if (*n > *(n + 1)) {
+
+        int temp = *n;
+        *n = *(n + 1);
+        *(n + 1) = temp;
+        swap = true;
+      }
+    }
+  }
+}
+
+void quick_sort(int *begin, int *end) {
+  if (begin == nullptr || end == nullptr) {
+    std::cout << "One of the argument is null\n";
+    return;
+  }
+  while (begin < end) {
+    int pivot = *end;
+    int *i = begin;
+    for (int *j = begin; j < end; ++j) {
+      if (*j < pivot) {
+        std::swap(*i, *j);
+        ++i;
+      }
+    }
+    std::swap(*i, *end);
+    if (i - begin <= end - i) {
+      quick_sort(begin, i - 1);
+      begin = i + 1;
+    } else {
+      quick_sort(i + 1, end);
+      end = i - 1;
+    }
+  }
+}
---
+++ problem2.cpp
@@ -0,0 +1,129 @@
+#include <algorithm>
+#include <iostream>
+
+union Arrays {
+  uint u[];
+  double d[];
+  char ch[];
+} array;
+
+enum class Data_Type { UINT = 1, DOUBLE = 2, CHAR = 3 };
+
+void fill_array_uint(Arrays &array, const size_t &Size_Array);
+void fill_array_double(Arrays &array, const size_t &Size_Array);
+void fill_array_char(Arrays &array, const size_t &Size_Array);
+bool predicat(char &a, char &b);
+void print_array(const Arrays &array, const size_t &Size_Array, Data_Type type);
+
+int main() {
+  std::cout << "Enter the size of the sortable array(positive integer)\n";
+
+  size_t size_array{};
+  std::cin >> size_array;
+  if (!std::cin) {
+    std::cout << "incorrect input\n";
+    return -1;
+  }
+  const size_t Size_Array = size_array;
+
+  std::cout << "Choose input data type:\n"
+            << "1 - unsigner integer;\n"
+            << "2 - double;\n"
+            << "3 - characters;\n";
+
+  uint choose{};
+  std::cin >> choose;
+
+  Data_Type type_choose = static_cast<Data_Type>(choose);
+
+  switch (type_choose) {
+  case Data_Type::UINT:
+    fill_array_uint(array, Size_Array);
+    std::sort(array.u, array.u + Size_Array);
+    print_array(array, Size_Array, type_choose);
+    break;
+  case Data_Type::DOUBLE:
+    fill_array_double(array, Size_Array);
+    std::sort(array.d, array.d + Size_Array);
+    print_array(array, Size_Array, type_choose);
+    break;
+  case Data_Type::CHAR:
+    fill_array_char(array, Size_Array);
+    std::sort(array.ch, array.ch + Size_Array, predicat);
+    print_array(array, Size_Array, type_choose);
+    break;
+  default:
+    std::cout << "incorrect input\n";
+    return -1;
+    break;
+  }
+
+  return 0;
+}
+
+void fill_array_uint(Arrays &array, const size_t &Size_Array) {
+  std::cout << "Enter " << Size_Array << " unsigned integers\n";
+  for (size_t i = 0; i < Size_Array; ++i) {
+    int var{};
+    std::cin >> var;
+    if (!std::cin || var < 0) {
+      std::cout << "incorrect input\n";
+      return;
+    }
+    array.u[i] = var;
+  }
+}
+void fill_array_double(Arrays &array, const size_t &Size_Array) {
+  std::cout << "Enter " << Size_Array << " real numbers\n";
+  for (size_t i = 0; i < Size_Array; ++i) {
+    double var{};
+    std::cin >> var;
+    if (!std::cin) {
+      std::cout << "incorrect input\n";
+      return;
+    }
+      array.d[i] = var;    
+  }
+}
+void fill_array_char(Arrays &array, const size_t &Size_Array) {
+  std::cout << "Enter " << Size_Array << " characters\n";
+  for (size_t i = 0; i < Size_Array; ++i) {
+    char var{};
+    std::cin >> var;
+    if (!std::cin) {
+      std::cout << "incorrect input\n";
+      return;
+    }
+      array.ch[i] = var;    
+  }
+}
+
+bool predicat(char &a, char &b) {
+  if (a >= '0' && a <= '9') {
+    return a > b;
+  }
+  if (b >= '0' && b <= '9') {
+    return a > b;
+  }
+  return a < b;
+}
+
+void print_array(const Arrays &array, const size_t &Size_Array,
+                 Data_Type type) {
+  if (type == Data_Type::UINT) {
+    for (size_t i = 0; i < Size_Array; ++i) {
+      std::cout << array.u[i] << " ";
+    }
+  }
+  if (type == Data_Type::DOUBLE) {
+    for (size_t i = 0; i < Size_Array; ++i) {
+      std::cout << array.d[i] << " ";
+    }
+  }
+  if (type == Data_Type::CHAR) {
+    for (size_t i = 0; i < Size_Array; ++i) {
+      std::cout << array.ch[i];
+    }
+  }
+  std::cout << '\n';
+}
