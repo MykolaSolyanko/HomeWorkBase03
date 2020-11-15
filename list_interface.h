281129bddc2e9a357d96f6aeb8ea886f7d38d101
GKuzzinshtern <72338984+GKuzzinshtern@users.noreply.github.com>
2020-11-15 14:51:01 +0200

    Changed formatting

--- list_interface.h
+++ list_interface.h
@@ -2,44 +2,54 @@
 
 #include <iostream>
 
-struct structNode{
-  structNode* oPrev{};
+struct structNode {
+  structNode *oPrev{};
   int iData;
-  structNode* oNext{};
+  structNode *oNext{};
 };
 
-struct structList{
-  structNode* oFirstNode{};
+struct structList {
+  structNode *oFirstNode{};
   size_t sizeSize{};
 };
 
-//Element access
-inline structNode* GetFirstEl(const structList& in_oList) {return in_oList.oFirstNode;};
-structNode* GetLastEl(const structList& in_oList);
+// Element access
+inline structNode *GetFirstEl(const structList &in_oList) {
+  return in_oList.oFirstNode;
+};
+structNode *GetLastEl(const structList &in_oList);
 
 // Inserting element
-void vPushFront(structList& in_oList, int in_iData);
-void vPushBack(structList& in_oList, int in_iData);
-bool bInsert(structList& in_oList, structNode* oCurrentNode, int in_iData);
+void vPushFront(structList &in_oList, int in_iData);
+void vPushBack(structList &in_oList, int in_iData);
+bool bInsert(structList &in_oList, structNode *oCurrentNode, int in_iData);
 
 // Deleting element
-bool bPopFront(structList& in_oList);
-bool bPopBack(structList& in_oList);
-bool bErase(structList& in_oList, structNode* oCurrentNode);
-void vClear(structList& in_oList);
+bool bPopFront(structList &in_oList);
+bool bPopBack(structList &in_oList);
+bool bErase(structList &in_oList, structNode *oCurrentNode);
+void vClear(structList &in_oList);
 
 // Compare
-inline bool bCmp(structNode* oLess, structNode* oMore) {return (oLess->iData < oMore->iData);}
-inline bool bEqual(structNode* oFirst, structNode* oSecond) {return (oFirst->iData == oSecond->iData);}
-inline bool bEqual(structNode* oFirst, int iSecond) {return (oFirst->iData == iSecond);}
+inline bool bCmp(structNode *oLess, structNode *oMore) {
+  return (oLess->iData < oMore->iData);
+}
+inline bool bEqual(structNode *oFirst, structNode *oSecond) {
+  return (oFirst->iData == oSecond->iData);
+}
+inline bool bEqual(structNode *oFirst, int iSecond) {
+  return (oFirst->iData == iSecond);
+}
 
 // Find element
-structNode* FindByValue(structList& in_oList, const int in_iValue);
+structNode *FindByValue(structList &in_oList, const int in_iValue);
 
 // Capacity
-inline size_t Size(const structList& in_oList) {return in_oList.sizeSize;};
-inline bool bIsEmpty(const structList& in_oList) {return (in_oList.sizeSize == 0);};
+inline size_t Size(const structList &in_oList) { return in_oList.sizeSize; };
+inline bool bIsEmpty(const structList &in_oList) {
+  return (in_oList.sizeSize == 0);
+};
 
 // Helpers
-bool bCheckNode(structList& in_oList, structNode* oNodeToCheck);
-void vPrintList(const structList& in_oList);
+bool bCheckNode(structList &in_oList, structNode *oNodeToCheck);
+void vPrintList(const structList &in_oList);
