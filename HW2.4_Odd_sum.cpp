2b08969a55c08a8d50712bc1cd093f26309eac7f
Roman <72319676+Zmeyznaet@users.noreply.github.com>
2020-11-26 15:06:33 +0200

    HW2.4_Odd_sum_version_2.cpp

--- HW2.4_Odd_sum.cpp
+++ HW2.4_Odd_sum.cpp
@@ -1,26 +1,30 @@
 #include <iostream>
 
 int main() {
-  std::cerr << "Please, enter the list of numbers (up to 50 numbers)" << '\n';
-  std::cerr << "Values of the numbers has to be in the range from -60 up to 90"
-            << '\n';
-  std::cerr << "If you want to finish, please, print 100" << '\n';
+  std::cout << "Enter a size of the number list" << std::endl;
+  size_t kSize{0};
+  if (!(std::cin >> kSize) || kSize > 50 || kSize < 1) {
+    std::cerr << "Wrong number. Try again" << std::endl;
+    return 0;
+  }
+  std::cout << "Enter the list of numbers" << std::endl;
+  std::cout << "Values of the numbers has to be in the range from -60 up to 90"
+            << std::endl;
   int odd_sum{0};
-  for (int i = 0; i < 50; ++i) {
-    int input_number{0};
-    std::cin >> input_number;
-    if (input_number == 100) {
-      break;
-    }
-    if (input_number < -60 || input_number > 90 || !(std::cin)) {
-      std::cerr << "Value of the number is out of the numeric range" << '\n';
+  for (size_t index = 0; index < kSize; ++index) {
+    int input_list[kSize]{};
+    constexpr int min_value{-60};
+    constexpr int max_value{90};
+    if (!(std::cin >> input_list[index]) || input_list[index] < min_value ||
+        input_list[index] > max_value) {
+      std::cerr << "Wrong number. Try again" << std::endl;
       return 0;
     }
-    if ((input_number & 1) != 1) {
+    if ((input_list[index] % 2) == 0) {
       continue;
     }
-    odd_sum += input_number;
+    odd_sum += input_list[index];
   }
-  std::cerr << "The sum of the odd numbers is " << odd_sum << '\n';
+  std::cout << "The sum of the odd numbers is " << odd_sum << '\n';
   return 0;
 }
