060209e362741fd98f2baae11099e8824b77a069
Roman <72319676+Zmeyznaet@users.noreply.github.com>
2020-11-26 17:49:50 +0200

    Update Roman_Sukhariev_3.4.cpp

--- Roman_Sukhariev_3.4.cpp
+++ Roman_Sukhariev_3.4.cpp
@@ -2,27 +2,20 @@
 #include <iostream>
 
 int main() {
-  uint64_t kSize{0};
-  std::cout << "Please, enter a number of symbols you want to use" << '\n';
-  std::cin >> kSize;
-  if (!(std::cin) || kSize == 0) {
-    std::cout << "You entered a wrong symbol. Please, try again" << '\n';
-    return 0;
-  }
-  ++kSize;  // increasing for '\0'
+  constexpr size_t kSize{5};
   char array[kSize]{};
-  std::cout << "Please, enter your string" << '\n';
+  std::cout << "Please, enter your string (up to 5 symbols)" << '\n';
   std::cin >> array;
-  for (uint64_t index = 0; index < kSize; ++index) {
-    if (array[index] > 96 && array[index] < 123) {
-      array[index] -= 32;
+  for (size_t index = 0; index < kSize; ++index) {
+    constexpr int first_letter{97};
+    constexpr int last_letter{122};
+    if (array[index] >= first_letter && array[index] <= last_letter) {
+      constexpr int uppercase_translation{32};
+      array[index] -= uppercase_translation;
     }
   }
-  for (uint64_t index = 0; index < kSize; ++index) {
-    if (array[index] == 0) {
-      std::cout << ' ';
-    }
-    std::cout << array[index];
+  for (char elem : array) {
+    std::cout << elem;
   }
   std::cout << '\n';
   return 0;
