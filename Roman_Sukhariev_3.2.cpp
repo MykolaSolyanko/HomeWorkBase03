2f469e6f200e316a53139d585ea27f88864b5ef1
Roman <72319676+Zmeyznaet@users.noreply.github.com>
2020-11-26 17:31:56 +0200

    Update Roman_Sukhariev_3.2.cpp

--- Roman_Sukhariev_3.2.cpp
+++ Roman_Sukhariev_3.2.cpp
@@ -1,16 +1,16 @@
-#include <cstdint>
+#include <array>
 #include <iostream>
 
-static void print_array(int array[], size_t kSize) {
-  for (size_t index = 0; index < kSize; ++index) {
-    std::cerr << array[index] << ' ';
+static void print_array(std::array<int, 5> &array) {
+  for (int elem : array) {
+    std::cout << elem << ' ';
   }
-  std::cerr << '\n';
+  std::cout << '\n';
   return;
 }
-static void bubble_sort(int array[], size_t kSize) {
-  for (size_t i = 0; i < kSize; ++i) {
-    for (size_t j = kSize - 1; j > i; --j) {
+static void bubble_sort(std::array<int, 5> &array) {
+  for (size_t i = 0; i < array.size(); ++i) {
+    for (size_t j = array.size() - 1; j > i; --j) {
       if (array[j - 1] > array[j]) {
         int32_t tmp = array[j - 1];
         array[j - 1] = array[j];
@@ -21,22 +21,17 @@ static void bubble_sort(int array[], size_t kSize) {
   return;
 }
 int main() {
-  size_t kSize{};
-  std::cerr << "Enter a size of array" << '\n';
-  if (!(std::cin >> kSize) || kSize < 1) {
-    std::cout << "Wrong size. Try again" << '\n';
-    return 0;
-  }
-  int array[kSize]{};
-  std::cerr << "Enter a list of numbers to fulfill the array (w)" << '\n';
+  std::cout << "Enter a list of numbers to fulfill the array (up to 5 numbers)"
+            << '\n';
+  constexpr size_t kSize{5};
+  std::array<int, kSize> array{};
   for (size_t index = 0; array[index] == 0;) {
-    if (!(std::cin >> array[index]) || array[index] == 0) {
-      std::cout << "Wrong number or symbol. Try again" << '\n';
-      array[index] = 0;
-      continue;
+    if (!(std::cin >> array[index])) {
+      std::cerr << "Wrong number or symbol. Try again" << '\n';
+      return 0;
     }
-    bubble_sort(array, kSize);
-    print_array(array, kSize);
+    bubble_sort(array);
+    print_array(array);
   }
   return 0;
 }
