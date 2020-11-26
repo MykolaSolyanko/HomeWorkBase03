21d187b64a45abd13fc7089166bc7b7c9ea4de02
Roman <72319676+Zmeyznaet@users.noreply.github.com>
2020-11-26 17:38:08 +0200

    Update Roman_Sukhariev_3.3.cpp

--- Roman_Sukhariev_3.3.cpp
+++ Roman_Sukhariev_3.3.cpp
@@ -1,39 +1,39 @@
 #include <array>
-#include <cstdint>
 #include <iostream>
 
 int main() {
-  constexpr uint32_t kSize{5};
+  constexpr size_t kSize{5};
   std::array<int, kSize> ar{};
-  std::cerr << "Please, fulfill the array. Use numbers 1..5. Zero is forbidden"
+  std::cout << "Please, fulfill the array. Use numbers 1..5. Zero is forbidden"
             << '\n';
-  for (int index = 0; index < kSize; ++index) {
-    std::cin >> ar[index];
-    if (!(std::cin) || ar[index] < 1 || ar[index] > 5) {
+  constexpr int max_value{5};
+  constexpr int min_value{1};
+  for (auto& value : ar) {
+    std::cin >> value;
+    if (!(std::cin) || value < min_value || value > max_value) {
       std::cerr << "You entered a wrong symbol. Try again" << '\n';
       return 0;
     }
   }
-  std::cerr << "Try to find all numbers of the array" << '\n';
-  std::cerr << "You can use numbers 1..5" << '\n';
-  for (int counter = 0; counter < kSize;) {
+  std::cout << "Try to find all numbers of the array" << '\n';
+  std::cout << "You can use numbers 1..5" << '\n';
+  for (int counter = 0; counter < ar.size();) {
     int tmp{0};
-    std::cin >> tmp;
-    if (!(std::cin)) {
+    if (!(std::cin >> tmp)) {
       std::cerr << "You entered a wrong symbol. Try again" << '\n';
       return 0;
     }
-    for (int index = 0; index < kSize; ++index) {
-      if (ar[index] != tmp) {
+    for (auto& value : ar) {
+      if (value != tmp) {
         continue;
       }
-      ar[index] = 0;
+      value = 0;
       ++counter;
     }
   }
-  std::cerr << "You found all numbers. Congratulations!" << '\n';
-  for (int index = 0; index < kSize; ++index) {
-    std::cerr << ar[index] << '\n';
+  std::cout << "You found all numbers. Congratulations!" << '\n';
+  for (auto& value : ar) {
+    std::cout << value << '\n';
   }
   return 0;
 }
