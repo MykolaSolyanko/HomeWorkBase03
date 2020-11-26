0e961ffc4bc5a7d9efc44a63df3e681cb1e297ac
Roman <72319676+Zmeyznaet@users.noreply.github.com>
2020-11-26 14:56:21 +0200

    HW2.1_Average_version_2.cpp

--- HW2.1_Average.cpp
+++ HW2.1_Average.cpp
@@ -1,19 +1,16 @@
-#include <cstdint>
 #include <iostream>
 
 int main() {
-  std::cerr << "Please, enter number" << std::endl;
+  std::cout << "Please, enter number" << std::endl;
   uint64_t number{0};
   std::cin >> number;
   int sum{0};
   int counter{0};
-  for (; number != 0;) {
+  for (; number != 0; number /= 10, ++counter) {
     sum += (number % 10);
-    number /= 10;
-    ++counter;
   }
-  std::cerr << "Here is the sum of the numbers: " << sum << '\n';
+  std::cout << "Here is the sum of the numbers: " << sum << '\n';
   float average{static_cast<float>(sum) / counter};
-  std::cerr << "Here is the average of the numbers: " << average << '\n';
+  std::cout << "Here is the average of the numbers: " << average << '\n';
   return 0;
 }
