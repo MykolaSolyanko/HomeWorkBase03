9f0c72b66565a148a5b81b850de56f25075286a7
Roman <72319676+Zmeyznaet@users.noreply.github.com>
2020-11-26 17:51:45 +0200

    Update Roman_Sukhariev_3.5.cpp

--- Roman_Sukhariev_3.5.cpp
+++ Roman_Sukhariev_3.5.cpp
@@ -1,21 +1,16 @@
 #include <iostream>
 
 int main() {
-  int32_t kSize{0};
-  std::cout << "Enter a number of symbols you want to use" << '\n';
-  std::cin >> kSize;
-  if (!(std::cin) || kSize < 1) {
-    std::cout << "You entered a wrong number. Try again" << '\n';
-    return 0;
-  }
-  ++kSize;
+  constexpr size_t kSize{5};
   char array[kSize]{};
   std::cout << "Enter your string" << '\n';
   std::cin >> array;
   std::cout << "is diget ";
-  for (int index = 0; index < kSize; ++index) {
-    if (array[index] > 47 && array[index] < 58) {
-      std::cout << array[index] << ' ';
+  for (char elem : array) {
+    constexpr int zero_number{48};
+    constexpr int nine_number{57};
+    if (elem >= zero_number && elem <= nine_number) {
+      std::cout << elem << ' ';
     }
   }
   std::cout << '\n';
