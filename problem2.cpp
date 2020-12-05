646f6dc6e94fd5d58855b42344ded69daf08cbc1
Dmytro Filippov <70589756+dim-Fi@users.noreply.github.com>
2020-12-05 17:39:29 +0200

    Update problem2.cpp

--- problem2.cpp
+++ problem2.cpp
@@ -1,47 +1,98 @@
-~/develp/ITEA/homework5 ❯                                                                                    17:14:
-0❯ clang++ -std=c++17 -Wall problem2.cpp
-❯ ./a.out
-Enter the string of the positive integers
+#include <iostream>
 
-Enter the size piece of the string
+class Stack_Singleton {
+public:
+  static Stack_Singleton& get_instance() {
+    static Stack_Singleton stack{};
+    return stack;
+  }
 
+  bool push_stack(int a);
+  bool pop_stack();
+  bool empty_stack() const { return current < 0; }
+  int top_stack() const;
+  void print_stack() const;
 
+private:
+  inline static int *array{nullptr};
+  static constexpr size_t Size_Array{10};
+  int current{-1};
 
+  Stack_Singleton();
+  ~Stack_Singleton();
+  Stack_Singleton(const Stack_Singleton &other) = delete;
+  Stack_Singleton operator=(const Stack_Singleton &other) = delete;
+};
 
+Stack_Singleton::Stack_Singleton() : current{-1} {
+  if (Size_Array > 0) {
+    array = new int[Size_Array];
+  }
+}
 
+Stack_Singleton::~Stack_Singleton() {
+  if (array != nullptr) {
+    delete[] array;
+  }
+}
 
+bool Stack_Singleton::push_stack(int a) {
+  if (Size_Array == current + 1 || array == nullptr) {
+      std::cout << "invalid operation\n";
+    return false;
+  }
+  array[++current] = a;
+  return true;
+}
 
+bool Stack_Singleton::pop_stack() {
+    if (current < 0 || array == nullptr) {
+	std::cout << "invalid operation\n";
+    return false;
+  } 
+  --current;
+  return true;
+}
 
+int Stack_Singleton::top_stack() const {
+  if (array == nullptr || Size_Array <= 0) {
+    std::cout << "invalid operation\n";
+    return 0;
+  }
+  return array[current];
+}
 
+void Stack_Singleton::print_stack() const {
+  if (!empty_stack() && array != nullptr) {
+    for (int i = 0; i <= current; ++i) {
+      std::cout << array[i] << ' ';
+    }
+    std::cout << '\n';
+  }
+}
 
-o
-Sorry.. incorrect size of piece
-[1]    7527 floating point exception (core dumped)  ./a.out
-❯ clang++ -std=c++17 -Wall problem2.cpp
-❯ ./a.out
-Enter the string of the positive integers
+int main() {
 
-String is empty
-❯ cd -
-~/develp/ITEA/homework5
-❯ cd
-❯ cd develp
-❯ cd ITEA
-❯ cd homework6
-❯ clang++ -std=c++17 -Wall -pedantic problem1.cpp
-❯ ./a.out
-List l: 250 200 27 100 120 
-Deleting first element l
-200 27 100 120 
-Create list l2 copy l1
-First element l2 = 200
-Last element l2 = 120
-List l2: 200 27 100 120 
-List l2 has 4 elements
-❯ 
-❯ clang++ -std=c++17 -Wall -pedantic problem2.cpp
-❯ ./a.out
-Stack st contains: 100 -25 50 
-Stack st2 contains: 100 -25 200 
-Top st2 equal: 200
-~/develp/ITEA/homework6 ❯                                                                                 18:07:15
+  Stack_Singleton &st = Stack_Singleton::get_instance();
+
+  st.push_stack(100);
+  st.push_stack(150);
+  st.pop_stack();
+  st.push_stack(-25);
+  st.push_stack(50);
+
+  std::cout << "Stack st contains: ";
+  st.print_stack();
+
+  Stack_Singleton &st2 = Stack_Singleton::get_instance();
+
+  st2.pop_stack();
+  st2.push_stack(200);
+
+  std::cout << "Stack st2 contains: ";
+  st.print_stack();
+
+  std::cout << "Top st2 equal: " << st2.top_stack() << '\n';
+
+  return 0;
+}
